adb142dc6b9e0e2982f9dd3f6c36bfe132da04e0
Zosia <zosiacastiel@gmail.com>
2023-04-21 18:17:45 +0200

    schedules are being displayed in a table

--- website/php_functions/schedule_function.php
+++ website/php_functions/schedule_function.php
@@ -7,6 +7,22 @@ function schedule()
     $semester = $_SESSION['semester'];
     $conn = connection();
     echo $field_of_study . " " . $semester;
+
+    $query = "SELECT id FROM fields_of_study WHERE name = '$field_of_study'";
+    $result = mysqli_query($conn, $query);
+    $row = mysqli_fetch_assoc($result);
+    $field_of_study_id = $row['id'];
+
+    $query = "SELECT subject, day, beginning_time, end_time FROM schedules WHERE field_of_study_id = '$field_of_study_id' AND semester = '$semester'";
+    $result = mysqli_query($conn, $query);
+
+    $hours = array();
+    for ($h = 8; $h <= 20; $h++) {
+        for ($m = 0; $m < 60; $m += 15) {
+            $hours[] = sprintf('%02d:%02d', $h, $m);
+        }
+    }
+
     echo "<table class=\"table table-bordered\">
     <thead>
         <tr>
@@ -19,8 +35,27 @@ function schedule()
         </tr>
     </thead>
     <tbody id=\"tableBody\">";
+
+    foreach ($hours as $hour) {
+        echo "<tr><td>$hour</td>";
+        for ($d = 1; $d <= 5; $d++) {
+            $rowspan = 1;
+            $cell_content = '';
+            while ($schedule = mysqli_fetch_assoc($result)) {
+                if (intval($schedule['day']) == $d && strtotime($schedule['beginning_time']) == strtotime($hour)) {
+                    $end_time = $schedule['end_time'];
+                    $rowspan = ceil((strtotime($end_time) - strtotime($hour)) / 900);
+                    $cell_content = $schedule['subject'];
+                    break; 
+                }
+            }
+            echo "<td" . ($rowspan > 1 ? " rowspan=\"$rowspan\"" : "") . ">$cell_content</td>";
+            mysqli_data_seek($result, 0);
+        }
+        echo "</tr>";
+    }
+
     echo "</tbody>
     </table>";
 }
-
 ?>
