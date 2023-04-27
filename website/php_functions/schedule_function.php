082b948763989493f508f103a6e6401ff1ace322
Zosia <zosiacastiel@gmail.com>
2023-04-28 00:03:45 +0200

    handling colspans and rowspans in table

--- website/php_functions/schedule_function.php
+++ website/php_functions/schedule_function.php
@@ -6,14 +6,14 @@ function schedule()
     $field_of_study = $_SESSION['major'];
     $semester = $_SESSION['semester'];
     $conn = connection();
-    echo $field_of_study . " " . $semester;
+    //echo $field_of_study . " " . $semester;
 
     $query = "SELECT id FROM fields_of_study WHERE name = '$field_of_study'";
     $result = mysqli_query($conn, $query);
     $row = mysqli_fetch_assoc($result);
     $field_of_study_id = $row['id'];
 
-    $query = "SELECT subject, day, beginning_time, end_time FROM schedules WHERE field_of_study_id = '$field_of_study_id' AND semester = '$semester'";
+    $query = "SELECT subject, day, beginning_time, end_time, type FROM schedules WHERE field_of_study_id = '$field_of_study_id' AND semester = '$semester'";
     $result = mysqli_query($conn, $query);
 
     $hours = array();
@@ -27,34 +27,96 @@ function schedule()
     <thead>
         <tr>
             <th></th>
-            <th class=\"col-md-2\">Poniedziałek</th>
-            <th class=\"col-md-2\">Wtorek</th>
-            <th class=\"col-md-2\">Środa</th>
-            <th class=\"col-md-2\">Czwartek</th>
-            <th class=\"col-md-2\">Piątek</th>
+            <th class=\"col-md-2\" colspan=\"12\">Poniedziałek</th>
+            <th class=\"col-md-2\" colspan=\"12\">Wtorek</th>
+            <th class=\"col-md-2\" colspan=\"12\">Środa</th>
+            <th class=\"col-md-2\" colspan=\"12\">Czwartek</th>
+            <th class=\"col-md-2\" colspan=\"12\">Piątek</th>
         </tr>
     </thead>
     <tbody id=\"tableBody\">";
-
+    $cell_type = '';
     foreach ($hours as $hour) {
+        $cell_type = '';
         echo "<tr><td>$hour</td>";
         for ($d = 1; $d <= 5; $d++) {
-            $rowspan = 1;
             $cell_content = '';
+            $colspan = 12;
+            $rowspan = 0;
+            $cell_added = false;
+            $conflicts = array();
             while ($schedule = mysqli_fetch_assoc($result)) {
                 if (intval($schedule['day']) == $d && strtotime($schedule['beginning_time']) == strtotime($hour)) {
                     $end_time = $schedule['end_time'];
-                    $rowspan = ceil((strtotime($end_time) - strtotime($hour)) / 900);
-                    $cell_content = $schedule['subject'];
-                    break; 
+                    $conflicts[] = $schedule;
                 }
             }
-            echo "<td" . ($rowspan > 1 ? " rowspan=\"$rowspan\"" : "") . ">$cell_content</td>";
+            $num_conflicts = count($conflicts);
+            if ($num_conflicts > 0) {
+                $colspan = floor(12 / $num_conflicts);
+                $rowspan = ceil((strtotime($conflicts[0]['end_time']) - strtotime($hour)) / 900);
+                $cell_content = $conflicts[0]['subject'];
+                $cell_type = $conflicts[0]['type'];
+                $cell_added = true;
+            }
+            $class_name = '';
+            switch ($cell_type) {
+                case 'seminar':
+                    $class_name = 'table-warning';
+                    break;
+                case 'labolatories':
+                    $class_name = 'table-success';
+                    break;
+                case 'lecture':
+                    $class_name = 'table-primary';
+                    break;
+                default:
+                    $class_name = '';
+                    break;
+            }
+            if ($cell_content) {
+                echo "<td" . ($rowspan > 1 ? " rowspan=\"$rowspan\"" : "") . " colspan=\"$colspan\" class=\"$class_name\" style='text-align:center;'>$cell_content</td>";
+            } else {
+                echo "<td" . ($rowspan > 1 ? " rowspan=\"$rowspan\"" : "") . " colspan=\"$colspan\"></td>";
+            }
+
+            if ($num_conflicts > 1) {
+                for ($i = 1; $i < $num_conflicts; $i++) {
+                    $rowspan = ceil((strtotime($conflicts[$i]['end_time']) - strtotime($hour)) / 900);
+                    $cell_content = $conflicts[$i]['subject'];
+                    $cell_type = $conflicts[$i]['type'];
+                    $class_name = '';
+                    switch ($cell_type) {
+                        case 'seminar':
+                            $class_name = 'table-warning';
+                            break;
+                        case 'labolatories':
+                            $class_name = 'table-success';
+                            break;
+                        case 'lecture':
+                            $class_name = 'table-primary';
+                            break;
+                        default:
+                            $class_name = '';
+                            break;
+                    }
+                    if ($cell_content) {
+                        echo "<td rowspan=\"$rowspan\" colspan=\"$colspan\" class=\"$class_name\" style='text-align:center;'>$cell_content</td>";
+                    } else {
+                        echo "<td rowspan=\"$rowspan\" colspan=\"$colspan\"></td>";
+                    }
+                }
+                $cell_added = true;
+            }
+
+            if (!$cell_added) {
+                //echo "<td colspan=\"12\"></td>";
+            }
+
             mysqli_data_seek($result, 0);
         }
         echo "</tr>";
     }
-
     echo "</tbody>
     </table>";
 }
